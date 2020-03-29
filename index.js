aa5930b8644e49e51cd3691382ad595bde6980ea
Wilford Madrazo <wilford_madrazo@dlsu.edu.ph>
2020-03-29 16:44:59 +0800

    Fixed contactus and faqs pages, and changed hover css for home cards and schedules reserve button.

--- index.js
+++ index.js
@@ -54,8 +54,41 @@ app.get('/contactus', function(req, res) {
     // The render function takes the template filename (no extension - that's what the config is for!)
     // and an object for what's needed in that template
     res.render('contactus', {
+      address: 'Rockwell Drive, Estrella, 4114 Makati',
+      title: 'W&Js Cinemas',
+      landline: '7777-1234',
+      mobile: '09171234567',
+      email: 'wjscinemas@gmail.com',
+
+      q1: 'Can I cancel my reservation anytime?',
+      a1: 'You may only cancel within 45 minutes of your current booking.',
+      q2: 'Is it possible to change my current booking?',
+      a2: 'No, the only way to do this is to cancel the current booking and make a new one.',
+      q3: 'What is the next step after reserving?',
+      a3: 'After reserving, simply present your digital ticket/s either through your mobile devices or a printed copy at the Online Resevations Line at W&Js Cinemas and pay for your physical ticket/s.'
     })
 });
+
+// FAQs route
+app.get('/faqs', function(req, res) {
+    // The render function takes the template filename (no extension - that's what the config is for!)
+    // and an object for what's needed in that template
+    res.render('faqs', {
+      address: 'Rockwell Drive, Estrella, 4114 Makati',
+      title: 'W&Js Cinemas',
+      landline: '7777-1234',
+      mobile: '09171234567',
+      email: 'wjscinemas@gmail.com',
+
+      q1: 'Can I cancel my reservation anytime?',
+      a1: 'You may only cancel within 45 minutes of your current booking.',
+      q2: 'Is it possible to change my current booking?',
+      a2: 'No, the only way to do this is to cancel the current booking and make a new one.',
+      q3: 'What is the next step after reserving?',
+      a3: 'After reserving, simply present your digital ticket/s either through your mobile devices or a printed copy at the Online Resevations Line at W&Js Cinemas and pay for your physical ticket/s.'
+    })
+});
+
 
 // 1917 reserve route
 app.get('/reserve-1917', function(req,res) {
