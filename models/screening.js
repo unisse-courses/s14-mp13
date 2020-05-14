1bc9671bc05d0b2f601f0fac76f19c81121ab10f
Wilford Madrazo <wil.jacobmadrazo@gmail.com>
2020-05-14 16:13:10 +0800

    Deleted unnecessary route files

--- models/screening.js
+++ models/screening.js
@@ -2,6 +2,7 @@ const mongoose = require('./connection');
 
 const ScreeningSchema = new mongoose.Schema(
   {
+    screeningid: {type: String}, // This is kept to keep track of screenings in mongoDB Atlas
     movie: {type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true},
     cinema: {type: mongoose.Schema.Types.ObjectId, ref: 'Cinema', required: true},
     timeslot: {type: String, required: true},
